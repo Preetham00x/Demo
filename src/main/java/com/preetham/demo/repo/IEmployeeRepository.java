6824dd6041468a3d236a3e3a8ffe2c675c8e640d
Preetham00x <preetham2006@outlook.com>
2026-07-21 22:56:42 +0530

    implementing custom finder methods in repository layer

--- src/main/java/com/preetham/demo/repo/IEmployeeRepository.java
+++ src/main/java/com/preetham/demo/repo/IEmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.preetham.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.preetham.demo.model.Employee;
 
 public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {
+	List<Employee> findByName(String name);
 
 }
