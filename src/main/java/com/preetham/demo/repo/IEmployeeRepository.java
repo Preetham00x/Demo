213329f0b194c8ff34561338942258201f7d7dfc
Preetham00x <preetham2006@outlook.com>
2026-07-22 22:13:09 +0530

    implemented JpaSpecificationExecutor  onto the application

--- src/main/java/com/preetham/demo/repo/IEmployeeRepository.java
+++ src/main/java/com/preetham/demo/repo/IEmployeeRepository.java
@@ -3,10 +3,11 @@ package com.preetham.demo.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.preetham.demo.model.Employee;
 
-public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {
+public interface IEmployeeRepository extends JpaRepository<Employee, Integer>,JpaSpecificationExecutor<Employee> {
 	List<Employee> findByName(String name);
 	List<Employee> findByDeptId(Integer deptId);
 	List<Employee> findBySalaryGreaterThan(Double Salary);
